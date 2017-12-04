26f0dd72b16abaf2d98e32ca3a20099435ab4f62
Leone Corrêa <leone.analistadesistemas@outlook.com.br>
2017-12-04 13:02:18 -0200

    Update dashboard.module.ts

--- v1.0/web/src/app/dashboard/dashboard.module.ts
+++ v1.0/web/src/app/dashboard/dashboard.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-// tslint:disable-next-line:max-line-length
 import { MatAutocompleteModule, MatButtonModule, MatButtonToggleModule, MatPaginatorModule, MatCardModule, MatCheckboxModule, MatChipsModule, MatDatepickerModule, MatDialogModule, MatGridListModule, MatIconModule, MatInputModule, MatListModule, MatMenuModule, MatProgressBarModule, MatProgressSpinnerModule, MatRadioModule, MatSelectModule, MatSidenavModule, MatSliderModule, MatSortModule, MatSlideToggleModule, MatSnackBarModule, MatTableModule, MatTabsModule, MatToolbarModule, MatTooltipModule, MatFormFieldModule, MatExpansionModule, MatStepperModule } from '@angular/material';
 
 import { DashboardComponent } from './dashboard.component';
@@ -22,38 +21,8 @@ import { AngularFireAuth } from 'angularfire2/auth';
       apiKey: 'AIzaSyDpYVG9Wn4I2YFHMp62HYeH9SBkGG_Q0nY'
     }),
     ChartsModule,
-    MatAutocompleteModule,
-    MatButtonModule,
-    MatButtonToggleModule,
-    MatPaginatorModule,
-    MatCardModule,
-    MatCheckboxModule,
-    MatChipsModule,
-    MatDatepickerModule,
-    MatDialogModule,
-    MatGridListModule,
-    MatIconModule,
-    MatInputModule,
-    MatListModule,
-    MatMenuModule,
-    MatProgressBarModule,
-    MatProgressSpinnerModule,
-    MatRadioModule,
-    MatSelectModule,
-    MatSidenavModule,
-    MatSliderModule,
-    MatSortModule,
-    MatSlideToggleModule,
-    MatSnackBarModule,
-    MatTableModule,
-    MatTabsModule,
-    MatToolbarModule,
-    MatTooltipModule,
-    MatFormFieldModule,
-    MatExpansionModule,
-    MatStepperModule,
-    AngularFireModule,
-    AngularFireModule.initializeApp(firebaseConfig)
+    MatAutocompleteModule, MatButtonModule, MatButtonToggleModule, MatPaginatorModule, MatCardModule,MatCheckboxModule, MatChipsModule, MatDatepickerModule, MatDialogModule, MatGridListModule, MatIconModule, MatInputModule, MatListModule, MatMenuModule, MatProgressBarModule, MatProgressSpinnerModule, MatRadioModule, MatSelectModule, MatSidenavModule, MatSliderModule, MatSortModule, MatSlideToggleModule, MatSnackBarModule, MatTableModule, MatTabsModule, MatToolbarModule, MatTooltipModule, MatFormFieldModule, MatExpansionModule, MatStepperModule,
+    AngularFireModule,AngularFireModule.initializeApp(firebaseConfig)
   ],
   declarations: [DashboardComponent],
   providers: [ AuthService ]
